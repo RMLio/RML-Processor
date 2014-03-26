99156c917c7a258f493087229e83d32c364b3f13
andimou <anastasia.dimou@ugent.be>
2014-03-26 20:40:24 +0100

    improved the month check

--- src/main/java/be/ugent/mmlab/rml/processor/concrete/CSS3Extractor.java
+++ src/main/java/be/ugent/mmlab/rml/processor/concrete/CSS3Extractor.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -32,7 +31,7 @@ public class CSS3Extractor extends AbstractRMLProcessor{
     
     private static Log log = LogFactory.getLog(RMLMappingFactory.class);
     private int enumerator;
-
+    
     @Override
     public void execute(SesameDataSet dataset, TriplesMap map, RMLPerformer performer, String fileName) {       
         //this should not be needed to be defined within the extractor
@@ -76,6 +75,13 @@ public class CSS3Extractor extends AbstractRMLProcessor{
         Jerry doc = Jerry.jerry(node.toString());
         List<String> list = new ArrayList();
         String replacement = null;
+        
+        String months[] = {"January", "February", "March", "April",
+                     "May", "June", "July", "August", "September",
+                     "October", "November", "December"};
+        String shortMonths[] = {"Jan", "Feb", "Mar", "Apr",
+                     "May", "June", "July", "Aug", "Sep",
+                     "Oct", "Nov", "Dec"};
        
         if(expression.equals("#")){
             list.add(Integer.toString(enumerator++));
@@ -159,55 +165,15 @@ public class CSS3Extractor extends AbstractRMLProcessor{
                             list.add(formatter.format(newInt).toString().trim());
                         }
                         
-                        else if (matcher.replaceAll(replacement).toString().contains("January"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("January", formatter.format(Calendar.JANUARY+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Jan"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Jan", formatter.format(Calendar.JANUARY+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("February"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("February", formatter.format(Calendar.FEBRUARY+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Feb"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Feb", formatter.format(Calendar.FEBRUARY+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("March"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("March", formatter.format(Calendar.MARCH+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Mar"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Mar", formatter.format(Calendar.MARCH+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("April"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("April", formatter.format(Calendar.APRIL+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Apr"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Apr", formatter.format(Calendar.APRIL+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("May"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("May", formatter.format(Calendar.MAY+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("June"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("June", formatter.format(Calendar.JUNE+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Jun"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Jun", formatter.format(Calendar.JUNE+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("July"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("July", formatter.format(Calendar.JULY+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Jul"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Jul", formatter.format(Calendar.JULY+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("August"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("August", formatter.format(Calendar.AUGUST+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Aug"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Aug", formatter.format(Calendar.AUGUST+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("September"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("September", formatter.format(Calendar.SEPTEMBER+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Sept"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Sept", formatter.format(Calendar.SEPTEMBER+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Sep"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Sep", formatter.format(Calendar.SEPTEMBER+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("October"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("October", formatter.format(Calendar.OCTOBER+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Oct"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Oct", formatter.format(Calendar.OCTOBER+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("November"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("November", formatter.format(Calendar.NOVEMBER+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Nov"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Nov", formatter.format(Calendar.NOVEMBER+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("December"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("December", formatter.format(Calendar.DECEMBER+1))));
-                        else if (matcher.replaceAll(replacement).toString().contains("Dec"))
-                            list.add(String.valueOf(matcher.replaceAll(replacement).replace("Dec", formatter.format(Calendar.DECEMBER+1))));
-                        else if (replace.toString().contains("Edited by:") || replace.toString().contains("CEURAUTHORS")){
+                        for(int i=0; i<11; i++)
+                            if( matcher.replaceAll(replacement).toString().contains(months[i]))
+                                list.add(String.valueOf(matcher.replaceAll(replacement).replace(months[i], formatter.format(i+1))));
+                         
+                        for(int i=0; i<11; i++)
+                            if( matcher.replaceAll(replacement).toString().contains(shortMonths[i]))
+                                list.add(String.valueOf(matcher.replaceAll(replacement).replace(shortMonths[i], formatter.format(i+1))));
+                        
+                        if (replace.toString().contains("Edited by:") || replace.toString().contains("CEURAUTHORS")){
                             values = matcher.replaceAll(replacement).split(",");
                             for(String val:values)
                                 list.add(StringEscapeUtils.unescapeHtml(val));
@@ -244,32 +210,24 @@ public class CSS3Extractor extends AbstractRMLProcessor{
     }
     
     private String checkforDate(String value){
+         String months[] = {"January", "February", "March", "April",
+                     "May", "June", "July", "August", "September",
+                     "October", "November", "December"};
+        String shortMonths[] = {"Jan", "Feb", "Mar", "Apr",
+                     "May", "June", "July", "Aug", "Sep",
+                     "Oct", "Nov", "Dec"};
+       
         DecimalFormat formatter = new DecimalFormat("00");
         
-        if (value.equals("January") || value.equals("Jan"))
-            value = String.valueOf(formatter.format(Calendar.JANUARY+1));
-        else if (value.equals("February") || value.equals("Feb"))
-            value = String.valueOf(formatter.format(Calendar.FEBRUARY+1));
-        else if (value.equals("March") || value.equals("Mar"))
-            value = String.valueOf(formatter.format(Calendar.MARCH+1));
-        else if (value.equals("April") || value.equals("Apr"))
-            value = String.valueOf(formatter.format(Calendar.APRIL+1));
-        else if (value.contains("May") || value.equals("May"))
-            value = String.valueOf(formatter.format(Calendar.MAY+1));
-        else if (value.equals("June") || value.equals("Jun"))
-            value = String.valueOf(formatter.format(Calendar.JUNE+1));
-        else if (value.equals("July") || value.equals("July"))
-            value = String.valueOf(formatter.format(Calendar.JULY+1));
-        else if (value.equals("August") || value.equals("Aug"))
-            value = String.valueOf(formatter.format(Calendar.AUGUST+1));
-        else if (value.equals("September") || value.equals("Sep") || value.equals("Sept"))
-            value = String.valueOf(formatter.format(Calendar.SEPTEMBER+1));
-        else if (value.equals("October") || value.equals("Oct"))
-            value = String.valueOf(formatter.format(Calendar.OCTOBER+1));
-        else if (value.equals("November") || value.equals("Nov"))
-            value = String.valueOf(formatter.format(Calendar.NOVEMBER+1));
-        else if (value.equals("December") || value.equals("Dec"))
-            value = String.valueOf(formatter.format(Calendar.DECEMBER+1));
+        for(int i=0; i<11; i++)
+            if( value.contains(months[i]))
+                value = String.valueOf(formatter.format(i+1));
+                         
+        for(int i=0; i<11; i++)
+            if( value.contains(shortMonths[i]) )
+                value = String.valueOf(formatter.format(i+1));
+                        
+        
        return value;
     }   
 }
