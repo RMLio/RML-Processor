978b52c6b7aeec0c9eef613281cc21466706702a
andimou <anastasia.dimou@ugent.be>
2015-05-05 19:20:15 +0200

    added process and split condition

---
+++ src/main/java/be/ugent/mmlab/rml/model/condition/ProcessCondition.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ugent.mmlab.rml.model.condition;
+
+/**
+ *
+ * @author andimou
+ */
+public interface ProcessCondition extends Condition{
+    
+}
