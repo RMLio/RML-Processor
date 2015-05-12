d11b6156ede38d33dbbcc3916ae197cc0751805f
andimou <anastasia.dimou@ugent.be>
2015-05-12 13:24:56 +0200

    bind condition

---
+++ src/main/java/be/ugent/mmlab/rml/model/condition/BindCondition.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ugent.mmlab.rml.model.condition;
+
+/**
+ *
+ * @author andimou
+ */
+public interface BindCondition extends Condition {
+
+    public String getReference();
+}
