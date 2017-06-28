2ea6f2b93ec9d2c930f613e8ba0c1261faa4aab8
Ben De Meester <ben.demeester@ugent.be>
2017-06-28 13:28:36 +0200

    no unneeded tests

--- src/test/java/be/ugent/mmlab/rml/MapperTest.java
+++ src/test/java/be/ugent/mmlab/rml/MapperTest.java
@@ -335,15 +335,15 @@ public class MapperTest
                 assertMap(fileToRMLFile, null, triplesMap)));
     }
 
-    public void testExampleFnSimilarity() {
-        URL fileToRMLFile = getClass().getResource(
-                "/exampleFn/mapping_similarity.rml.ttl");
-        URL fileToOutputFile = getClass().getResource(
-                "/exampleFn/example.output.ttl");
-        String[] triplesMap = {"http://example.com/test#Person_TemplateMapping"};
-        assertTrue(desiredOutput(fileToOutputFile).isEqualTo(
-                assertMap(fileToRMLFile, null, triplesMap)));
-    }
+//    public void testExampleFnSimilarity() {
+//        URL fileToRMLFile = getClass().getResource(
+//                "/exampleFn/mapping_similarity.rml.ttl");
+//        URL fileToOutputFile = getClass().getResource(
+//                "/exampleFn/example.output.ttl");
+//        String[] triplesMap = {"http://example.com/test#Person_TemplateMapping"};
+//        assertTrue(desiredOutput(fileToOutputFile).isEqualTo(
+//                assertMap(fileToRMLFile, null, triplesMap)));
+//    }
 
     public void testExampleFnUppercase() {
         URL fileToRMLFile = getClass().getResource(
@@ -355,15 +355,15 @@ public class MapperTest
                 assertMap(fileToRMLFile, null, triplesMap)));
     }
 
-    public void testExampleFnSubject() {
-        URL fileToRMLFile = getClass().getResource(
-                "/exampleFn/mapping_subject.rml.ttl");
-        URL fileToOutputFile = getClass().getResource(
-                "/exampleFn/example_subject.output.ttl");
-        String[] triplesMap = {"http://example.com/test#Person_TemplateMapping"};
-        assertTrue(desiredOutput(fileToOutputFile).isEqualTo(
-                assertMap(fileToRMLFile, null, triplesMap)));
-    }
+//    public void testExampleFnSubject() {
+//        URL fileToRMLFile = getClass().getResource(
+//                "/exampleFn/mapping_subject.rml.ttl");
+//        URL fileToOutputFile = getClass().getResource(
+//                "/exampleFn/example_subject.output.ttl");
+//        String[] triplesMap = {"http://example.com/test#Person_TemplateMapping"};
+//        assertTrue(desiredOutput(fileToOutputFile).isEqualTo(
+//                assertMap(fileToRMLFile, null, triplesMap)));
+//    }
 
     /*public void testExampleDBP() {
         URL fileToRMLFile = getClass().getResource(
